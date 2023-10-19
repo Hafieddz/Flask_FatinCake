0d1665046b0fde98f64ce36c524364e999ed9c30
Hafieddz <hasmy41@gmail.com>
2023-10-19 21:04:35 +0800

    feat : add new route for new page

--- app.py
+++ app.py
@@ -2,25 +2,47 @@ from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
-@app.route('/home')
+
+# Home Page
+@app.route('/')
 def index():
     return render_template('index.html')
 
+
+# Product Page
 @app.route('/products')
 def products():
     return render_template('all_products.html')
 
+# Base HTML
 @app.route('/base')
 def base():
     return render_template('base.html')
 
-@app.route('/')
-def custome_cake():
+# Custome Cake
+@app.route('/custome')
+def custome():
     return render_template('custome_cake.html')
 
+# Cart Page
 @app.route('/cart')
 def cart():
     return render_template('cart.html')
+
+# Just For Testing
+@app.route('/dump')
+def example():
+    return render_template('example_testing.html')
+
+# Login Page
+@app.route('/login')
+def login():
+    return render_template('index.html')
+
+# Detail Page
+@app.route('/details')
+def details():
+    return render_template('detail_product.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
