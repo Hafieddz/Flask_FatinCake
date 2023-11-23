3ec82b897ce73b3858640002e36991361aa3098b
Hafieddz <hasmy41@gmail.com>
2023-11-23 11:37:18 +0800

    feat : update the file field form where user cant input file except images

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField, ValidationError, BooleanField, TextAreaField
-from flask_wtf.file import FileField
+from flask_wtf.file import FileAllowed, FileField
 from wtforms.validators import DataRequired, EqualTo, Length, data_required, length
 
 # Register Form 
@@ -24,15 +24,15 @@ class UserForm(FlaskForm):
     first_name = StringField("Nama Depan", validators=[DataRequired()])
     last_name = StringField("Nama Belakang")
     phone_number = StringField("No Telepon", validators=[DataRequired()])
-    profile_pic = FileField("Profile Picture", validators=[DataRequired()])
+    profile_pic = FileField("Profile Picture", validators=[DataRequired(), FileAllowed(['jpg', 'png', 'jpeg'], 'Hanya bisa upload gambar!')])
     submit_form = SubmitField("Update Data")
-
+    
 # Update Product Form
 class UpdateForm(FlaskForm):
     nama = StringField("Nama Kue", validators=[DataRequired()])
     harga = StringField("Harga Kue", validators=[DataRequired()])
     varian = StringField("Varian", validators=[DataRequired()])
-    foto = FileField("Foto Produk", validators=[DataRequired()])
+    foto = FileField("Foto Produk", validators=[DataRequired(), FileAllowed(['jpg', 'png', 'jpeg'], 'Hanya bisa upload gambar!')])
     ukuran = StringField("Ukuran", validators=[DataRequired()])
     detail = TextAreaField("Detail", validators=[DataRequired()])
     submit_update = SubmitField("Update")
@@ -42,7 +42,7 @@ class AddForm(FlaskForm):
     nama = StringField("Nama Kue", validators=[DataRequired()])
     harga = StringField("Harga Kue", validators=[DataRequired()])
     varian = StringField("Varian", validators=[DataRequired()])
-    foto = FileField("Foto Produk", validators=[DataRequired()])
+    foto = FileField("Foto Produk", validators=[DataRequired(), FileAllowed(['jpg', 'png', 'jpeg'], 'Hanya bisa upload gambar!')])
     ukuran = StringField("Ukuran", validators=[DataRequired()])
     detail = TextAreaField("Detail", validators=[DataRequired()])
     submit_add = SubmitField("Tambah") 
